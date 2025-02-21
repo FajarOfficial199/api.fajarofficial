093ebb56514e30677485e71a48996466de6968e8
FajarOfficial199 <fajarshidik709@gmail.com>
2025-02-21 19:50:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const { youtube } = require("btch-downloader");
 const ytdl = require("@distube/ytdl-core");
 const { randomBytes } = require('crypto');
 const fs = require("fs");
+const isImageURL = require('image-url-validator').default
+const Canvas = require('canvas')
 const util = require('minecraft-server-util');
 const malScraper = require('mal-scraper');
 
@@ -645,6 +647,147 @@ app.get("/api/downloader/ytmp4", async (req, res) => {
         res.status(500).json({ status: false, message: "Error fetching video info", error: error.message });
     }
 });
+
+app.get('/api/maker/welcome1', async (req, res, next) => {
+	var name = req.query.name
+    var grup = req.query.gpname
+    var member = req.query.member
+	var pp = req.query.pp
+    var bg = req.query.bg
+	
+	var imgpp = await isImageURL(pp)
+	var bgimg = await isImageURL(bg)
+
+    if (!name ) return res.json({ status : false, creator : 'Alip', message : "[!] masukan parameter name"})  
+	if (!grup ) return res.json({ status : false, creator : 'Alip', message : "[!] masukan parameter gpname"})  
+    if (!member ) return res.json({ status : false, creator : 'Alip', message : "[!] masukan parameter member"})  
+	if (!pp ) return res.json({ status : false, creator : 'Alip', message : "[!] masukan parameter pp"})  
+    if (!bg ) return res.json({ status : false, creator : 'Alip', message : "[!] masukan parameter bg"})  
+
+	if ( !imgpp ) return res.json({ status : false, creator : 'Alip', message : "[!] cek kembali url image pp"}) 
+	if ( !bgimg ) return res.json({ status : false, creator : 'Alip', message : "[!] cek kembali url image bg"}) 
+   
+    Canvas.registerFont('./asset/font/Creme.ttf', { family: 'creme' })
+
+var welcomeCanvas = {}
+welcomeCanvas.create = Canvas.createCanvas(1024, 500)
+welcomeCanvas.context = welcomeCanvas.create.getContext('2d')
+welcomeCanvas.context.font = '72px creme'
+welcomeCanvas.context.fillStyle = '#ffffff'
+
+await Canvas.loadImage("./asset/image/wbg1.jpg").then(async (img) => {
+    welcomeCanvas.context.drawImage(img, 0, 0, 1024, 500)
+
+})
+
+let can = welcomeCanvas
+
+await Canvas.loadImage(bg)
+.then(bg => {
+can.context.drawImage(bg, 320, 0, 709, 360)
+})
+
+    let canvas = welcomeCanvas
+    canvas.context.beginPath()
+    canvas.context.arc(174, 279, 115, 0, Math.PI * 2, true)
+    canvas.context.stroke()
+    canvas.context.fill()
+    canvas.context.font = '100px creme',
+    canvas.context.textAlign = 'center'
+    canvas.context.fillText("Welcome", 670, 140)
+    canvas.context.font = '100px Helvetica'
+    canvas.context.fillText("____   ____", 670, 160)
+    canvas.context.fillText("✩", 670, 215)
+    canvas.context.font = '100px creme'
+    canvas.context.fillText(shortText(grup, 17), 670, 300)
+    canvas.context.font = '40px creme'
+    canvas.context.textAlign = 'start'
+    canvas.context.fillText(shortText(name, 40), 420, 420)
+    canvas.context.font = '35px creme'
+    canvas.context.fillText(`${shortText(member, 10)} th member`, 430, 490)
+    canvas.context.beginPath()
+    canvas.context.arc(174, 279, 110, 0, Math.PI * 2, true)
+    canvas.context.closePath()
+    canvas.context.clip()
+    await Canvas.loadImage(pp)
+    .then(pp => {
+        canvas.context.drawImage(pp, 1, 150, 300, 300)
+    })
+
+    res.set({'Content-Type': 'image/png'})
+    res.send(canvas.create.toBuffer())
+})
+
+
+app.get('/api/maker/goodbye1', async (req, res, next) => {
+	var name = req.query.name
+    var grup = req.query.gpname
+	var pp = req.query.pp
+    var member = req.query.member
+    var bg = req.query.bg
+
+	var imgpp = await isImageURL(pp)
+	var bgimg = await isImageURL(bg)
+
+    if (!name ) return res.json({ status : false, creator : 'Alip', message : "[!] masukan parameter name"})  
+	if (!grup ) return res.json({ status : false, creator : 'Alip', message : "[!] masukan parameter gpname"})  
+    if (!member ) return res.json({ status : false, creator : 'Alip', message : "[!] masukan parameter member"})  
+    if (!bg ) return res.json({ status : false, creator : 'Alip', message : "[!] masukan parameter bg"})  
+	if (!pp) return res.json({ status : false, creator : 'Alip', message : "[!] masukan parameter pp"}) 
+   
+	if ( !imgpp ) return res.json({ status : false, creator : 'Alip', message : "[!] masukan parameter pp Link pp dengan betul"}) 
+	if ( !bgimg ) return res.json({ status : false, creator : 'Alip', message : "[!] masukan parameter pp Link bg dengan betul"}) 
+
+    Canvas.registerFont('./asset/font/Creme.ttf', { family: 'creme' })
+
+var goobyeCanvas = {}
+goobyeCanvas.create = Canvas.createCanvas(1024, 500)
+goobyeCanvas.context =  goobyeCanvas.create.getContext('2d')
+goobyeCanvas.context.font = '72px creme'
+goobyeCanvas.context.fillStyle = '#ffffff'
+
+await Canvas.loadImage("./asset/image/wbg1.jpg").then(async (img) => {
+	goobyeCanvas.context.drawImage(img, 0, 0, 1024, 500)
+
+})
+
+let can =  goobyeCanvas
+
+await Canvas.loadImage(bg)
+.then(bg => {
+can.context.drawImage(bg, 320, 0, 709, 360)
+})
+
+    let canvas = goobyeCanvas
+    canvas.context.beginPath()
+    canvas.context.arc(174, 279, 115, 0, Math.PI * 2, true)
+    canvas.context.stroke()
+    canvas.context.fill()
+    canvas.context.font = '100px creme',
+    canvas.context.textAlign = 'center'
+    canvas.context.fillText("GoodBye", 670, 140)
+    canvas.context.font = '100px Helvetica'
+    canvas.context.fillText("____   ____", 670, 160)
+    canvas.context.fillText("✩", 670, 215)
+    canvas.context.font = '100px creme'
+    canvas.context.fillText(shortText(grup, 17), 670, 300)
+    canvas.context.font = '40px creme'
+    canvas.context.textAlign = 'start'
+    canvas.context.fillText(shortText(name, 40), 420, 420)
+    canvas.context.font = '35px creme'
+    canvas.context.fillText(`${shortText(member, 10)} th member`, 430, 490)
+    canvas.context.beginPath()
+    canvas.context.arc(174, 279, 110, 0, Math.PI * 2, true)
+    canvas.context.closePath()
+    canvas.context.clip()
+    await Canvas.loadImage(pp)
+    .then(pp => {
+        canvas.context.drawImage(pp, 1, 150, 300, 300)
+    })
+
+    res.set({'Content-Type': 'image/png'})
+    res.send(canvas.create.toBuffer())
+})
 
 // Route utama untuk halaman index.html
 app.get("/", (req, res) => {
