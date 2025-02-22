6f38bb5e6ca056f10d8f997de32a7606b71f4d72
FajarOfficial199 <fajarshidik709@gmail.com>
2025-02-23 06:39:26 +0700

    Update ytmp4.js

--- function/scraper/ytmp4.js
+++ function/scraper/ytmp4.js
@@ -10,19 +10,8 @@ async function ytVideo(url) {
             title: $(".info .title").text().trim(),
             thumbnail: $(".info img").attr("src"),
             duration: $(".info .duration").text().replace("Duration:", "").trim(),
-            downloads: []
+            videoUrl: $("#video-downloads .downloadsTable tbody tr:first-child td:nth-child(4) a").attr("href") || "",
         };
-
-        $("#video-downloads .downloadsTable tbody tr").each((i, el) => {
-            const quality = $(el).find("td:nth-child(1)").text().trim();
-            const format = $(el).find("td:nth-child(2)").text().trim();
-            const size = $(el).find("td:nth-child(3)").text().trim();
-            const link = $(el).find("td:nth-child(4) a").attr("href");
-
-            if (link) {
-                videoDetails.downloads.push({ quality, format, size, link });
-            }
-        });
 
         return videoDetails;
     } catch (error) {
